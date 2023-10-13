bbe677323e11b8996e4d2e9750e1c958a02c31bd
Doug Sillars <dougsillars@Dougs-MacBook-Air.local>
2023-10-13 05:59:21 -0400

    packages added, sections logger to 5s intervals

--- server/src/sections/sections.controller.ts
+++ server/src/sections/sections.controller.ts
@@ -6,12 +6,20 @@ import { SectionsService } from './sections.service'
 @Controller('sections')
 export class SectionsController {
   private readonly logger = new Logger(SectionsController.name)
+  private lastLogTime: number = 0;
+  private logInterval: number = 5000; // Log every 5 seconds
 
   constructor(private sectionsService: SectionsService) {}
 
   @Get()
   getAllSections(): Promise<SectionEntity[]> {
-    this.logger.log('GET /sections')
+
+    const currentTime = Date.now();
+
+    if (currentTime - this.lastLogTime >= this.logInterval) {
+      this.logger.log('GET /sections')
+      this.lastLogTime = currentTime;
+    }
 
     return this.sectionsService.findAll()
   }
